786a37b71bfe399fe8badd8de587342f5c8a0781
Tammy <tspeed90@gmail.com>
2020-07-16 16:01:08 -0700

    add link to switch between album view and edit page

--- src/views/AlbumDetails.vue
+++ src/views/AlbumDetails.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="album-details-container">
     <Album v-if="this.$route.name == 'Album Details'" :record="displayRecord" />
-    <AlbumEdit v-if="this.$route.name == 'Album Edit'" :record="displayRecord"/>  
+    <AlbumEdit v-if="this.$route.name == 'Album Edit'" :record="displayRecord"/> 
+    <router-link :to="editAlbumRoute">
+      <font-awesome-icon :icon="editLinkIcon" class="edit-icon" title="edit" />
+    </router-link>
   </div>
 </template>
 
@@ -10,9 +13,13 @@
 import { Component, Vue } from "vue-property-decorator";
 import Album from "@/components/Album.vue";
 import AlbumEdit from "@/components/AlbumEdit.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPencilAlt, faCheck } from "@fortawesome/free-solid-svg-icons";
 import firebase from "firebase/app";
 import 'firebase/database';
 const database = firebase.database();
+
+library.add(faPencilAlt, faCheck);
 
 let catalogData = Vue.observable({
   album:{}
@@ -30,13 +37,54 @@ export default class AlbumDetails extends Vue {
     database.ref('/albums/' + this.$route.params.id).on('value', function(snapshot) {
       catalogData.album = snapshot.val();
     });
-    console.log(this.$route.name)
   }
 
   get displayRecord() {
     return catalogData.album;
   }
+
+  get editAlbumRoute() {
+    if (this.$route.name == 'Album Details') {
+      return {name: 'Album Edit'};
+    } else {
+      return {name: 'Album Details'};
+    }
+  }
+
+  get editLinkIcon() {
+    if (this.$route.name == 'Album Details') {
+      return ['fas', 'pencil-alt'];
+    } else {
+      return ['fas', 'check'];
+    }
+  }
 }
 </script>
 
-<style lang="scss"></style>;
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+
+.album-details-container:first-child {
+  position: relative;
+}
+
+.edit-icon {
+  color: #333;
+  font-size: 30px;
+  border: 1px solid $theme-color;
+  background-color: $theme-color;
+  padding: 15px;
+  border-radius: 50%;
+  position: absolute;
+  right: 10%;
+  bottom: 3%;
+}
+
+@media screen and (min-width: 800px) {
+  .edit-icon {
+    right: 5%;
+    bottom: 5%;
+  }
+}
+
+</style>;
