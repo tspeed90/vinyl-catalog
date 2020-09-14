cfc7284613b7cc804dfce6e8949e712252419a52
Tammy <tspeed90@gmail.com>
2020-09-14 11:03:38 -0700

    expand and collapse decades collections

--- src/components/CollectionsList.vue
+++ src/components/CollectionsList.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="collections-list-container" v-if="Object.keys(catalogData.collectionTitles).length > 0">
     <router-link to="/collections" v-bind:key="all-albums" class="collections-buttons">all albums</router-link>
-    <div v-for="set in this.collectionSets" v-bind:key="set">
-      <router-link :to="{name: 'Collections List', params: { collection: title.displayName } }" v-for="title in set" v-bind:key="title" class="collections-buttons">{{title.displayName}}</router-link>
+    <div v-for="(set, belongsTo) in this.collectionSets" v-bind:key="set">
+      <button v-on:click="setExpandedState(belongsTo)" class="decades-collapse-btn" v-if="belongsTo !== ''">{{belongsTo}}<span class="expand-symbol">+</span></button>
+      <div v-if="isSetExpanded(belongsTo)">
+        <router-link :to="{name: 'Collections List', params: { collection: title.displayName } }" v-for="title in set" v-bind:key="title" class="collections-buttons">{{title.displayName}}</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +28,28 @@ export default class CollectionsList extends Vue {
     })
     return sets;
   }
+  
+  constructor() {
+    super();
+    this.collectionsState = {};
+    const collectionTitles = Object.values(this.catalogData.collectionTitles);
+      collectionTitles.forEach(title => {
+        if (title.belongsTo !== undefined) {
+          this.collectionsState[title.belongsTo] = false, 
+          this.collectionsState[""] = true
+        }
+      })
+  }
 
+  private setExpandedState(belongsTo:string) {
+    this.collectionsState[belongsTo] = !this.collectionsState[belongsTo];
+  }
+
+  private isSetExpanded(belongsTo:string) {
+    return this.collectionsState[belongsTo];
+  }
+
+ 
 }
 
 </script>
@@ -40,7 +64,7 @@ export default class CollectionsList extends Vue {
   justify-content: center;
 }
 
-.collections-buttons {
+.collections-buttons, .decades-collapse-btn {
   padding: 2px 5px;
   font-size: 20px;
   background-color: $theme-color;
@@ -52,6 +76,20 @@ export default class CollectionsList extends Vue {
   align-items: center;
   justify-content: center;
 }
+
+.decades-collapse-btn {
+  display: flex;
+  width: 95%;
+  text-align: center;
+  position: relative;
+}
+
+.expand-symbol {
+  position: absolute;
+  right: 5px;
+  top: 5px;
+}
+
 
 @media screen and (min-width: 700px) {
   .collections-list-container {
